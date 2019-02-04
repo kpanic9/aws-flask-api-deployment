d294edee207b30335045775cb02d3a5824f3868c
Namesh Sanjitha <tns.xiii@gmail.com>
2019-02-04 13:17:20 +0530

    identifying variables for parametrising the infrastructure build

---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "git_user" {
+	type = "string"
+	default = "user"
+}
+
+variable "git_password" {
+	type = "string"
+	default = "password"
+}
+
+variable "git_url" {
+	type = "string"
+	default = "url"
+}
+
+variable "api_dir" {
+	type = "string"
+	default = "url"
+}
+
+variable "config" {
+	type = "string"
+	default = "config"
+}
+
+variable "private_subnet_1" {
+	type = "string"
+	default = "subnet-0ada1a79ff65969ad"
+}
+
+variable "private_subnet_2" {
+	type = "string"
+	default = "subnet-03f5581803dd0663d"
+}
+
+variable "public_subnet_1" {
+	type = "string"
+	default = "subnet-0e01336213c9a67b4"
+}
