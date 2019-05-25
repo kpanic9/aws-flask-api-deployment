dccae80024b1f0cc303d54348b8eb2e98764ba0f
Namesh Sanjitha <tns.xiii@gmail.com>
2019-05-25 11:23:15 +0530

    included server configuration script

--- api.tf
+++ api.tf
@@ -2,3 +2,17 @@ provider "aws" {
 	region = "us-west-1"
 }
 
+
+
+data "template_file" "api_node" {
+	template = "${file("./app-config.sh.tpl")}"
+	
+	vars {
+		git_user = "${var.git_user}"
+		git_password = "${var.git_password}"
+		git_url = "${git_url}"
+		config = "${var.config}"
+		api_dir = "${var.api_dir}"
+	}
+}
+
